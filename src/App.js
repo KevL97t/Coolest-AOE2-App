01d4c174a7d2e2b8ef2d4acb06334e4f5e04ab26
Kevl97t <kalarboleda97@gmail.com>
2021-12-29 12:40:24 -0600

    Updated broswer router to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './Pages/Home/Home';
 import { Provider } from 'react-redux';
 import store from './redux/store';
